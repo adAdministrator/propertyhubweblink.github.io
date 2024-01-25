0a0ad45944edcad58b70d4336e36768250cc0053
adAdministrator <155119658+adAdministrator@users.noreply.github.com>
2024-01-25 03:37:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 //Load environment variable
 dotenv.config({ path: "./config/config.env" });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
