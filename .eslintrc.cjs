e65c6c606398861785696909395cf9e61d96dc65
adAdministrator <155119658+adAdministrator@users.noreply.github.com>
2024-01-25 17:57:43 +0100

    Your commit message

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,14 @@
-module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
-  },
-  "extends": [
+moduleexports= {
+  env: { browser: true, es2020: true },
+  extends: [
     "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime"
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  "overrides": [],
-  "globals": {
-    window: true,
-    module: true
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
   },
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
-  },
-  "plugins": ["react"],
-  "rules": {
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "double"],
-    "semi": ["error", "never"]
-  }
 }
